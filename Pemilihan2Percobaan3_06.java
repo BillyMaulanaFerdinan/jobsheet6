dc2af6cd2c83086d3a9240ff801951e7919133a0
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-10-12 14:33:21 +0700

    modifikasi equals

---
+++ Pemilihan2Percobaan3_06.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan3_06 {
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner(System.in);
+        String kategori;
+        int penghasilan, gajibersih;
+        double pajak = 0;
+
+        System.out.println("Masukkan Kategori: ");
+        kategori = input06.nextLine();
+        System.out.println("Masukkan Besarnya Penghasilan: ");
+        penghasilan = input06.nextInt();
+        if (kategori.equalsIgnoreCase("pekerja")){
+            if(penghasilan <= 2000000)
+            pajak = 0.1;
+            else if (penghasilan <= 3000000)
+            pajak = 0.15;
+            else
+            pajak = 0.2;
+        gajibersih = (int) (penghasilan - (pajak * penghasilan));
+        System.out.print("Penghasilan Bersih : " + gajibersih);
+        }
+        else if (kategori.equals("pebisnis")){
+            if (penghasilan <= 2500000)
+            pajak = 0.15;
+            else if (penghasilan <= 3000000)
+            pajak = 0.2;
+            else
+            pajak = 0.25;
+        gajibersih = (int) (penghasilan - (pajak * penghasilan));
+        System.out.print("Penghasilan Bersih : " + gajibersih);    
+        }
+        else
+        System.out.println("Masukan Kategori Salah");
+    }
+    
+    
+}
