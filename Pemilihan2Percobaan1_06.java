0ef34321d46c16f3d759e9010b62718fc5ef13ad
BillyMaulanaFerdinan <billy.maulana2020@gmail.com>
2023-10-12 15:40:05 +0700

    modif tahun 2000

--- Pemilihan2Percobaan1_06.java
+++ Pemilihan2Percobaan1_06.java
@@ -7,10 +7,13 @@ public class Pemilihan2Percobaan1_06{
         int tahun = input06.nextInt();
         if ((tahun % 4) == 0){
             if ((tahun % 100) !=0)
+                if ((tahun % 400) ==0)
             System.out.println("Tahun Kabisat");
-            else {
+                else {
             System.out.println("Bukan Tahun kabisat");    
-            }    
+            }
+            else
+            System.out.println("Tahun Kabisat");
         }
         else{
             System.out.println("Bukan Tahun Kabisat");
